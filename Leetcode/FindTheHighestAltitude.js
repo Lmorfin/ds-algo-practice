a532020942c53bc3e9dbb7f5c6586c75d5773c38
morfin <luismorfin64@gmail.com>
2023-06-21 20:06:42 -0700

    FindTheHighestAltitude

--- Leetcode/FindTheHighestAltitude.js
+++ Leetcode/FindTheHighestAltitude.js
@@ -2,6 +2,7 @@
 1732. Find the Highest Altitude
 Difficulty Rank: Easy
 
+Question: 
 There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. 
 The biker starts his trip on point 0 with altitude equal 0.
 You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). 
@@ -33,7 +34,7 @@ In this case the highest value is 1 [0,-5,-4,1,1,-6]
 
 const largestAltitude = (gain) => {
   let highestAltitude = 0;
-  altitude = [];
+  let altitude = [];
   altitude.push(0);
   for (let i = 0; i < gain.length; i++) {
     highestAltitude += gain[i];
